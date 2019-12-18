c23b3bb6810c613be75cc4144deee6fb465af29e
ilhxasll <liyanpeter@163.com>
2019-12-18 11:21:21 +0800

    Update systeminfo.go

--- plugins/inputs/systeminfo/systeminfo.go
+++ plugins/inputs/systeminfo/systeminfo.go
@@ -31,64 +31,64 @@ func (_ *SysInfoStats) Gather(acc telegraf.Accumulator) error {
 
 	bfRd := bufio.NewReader(f)
 	l := 0
-	var fields map[string]interface{}
-	fields = make(map[string]interface{})
+	var tags map[string]string
+	tags = make(map[string]string)
 	for {
 		line, err := bfRd.ReadString('\n')
 		a := strings.Split(line, "：")
 		if len(a) > 1 {
 			b := a[0]
-			var c string = a[1]
+			c := a[1]
 			if b == "测试" {
-				fields["测试"] = c
+				tags["测试"] = c
 			}
 			if b == "产品名称" {
-				fields["pro_name"] = c
+				tags["pro_name"] = c
 			}
 			if b == "标识码（产品唯一标识）" {
-				fields["pro_code"] = c
+				tags["pro_code"] = c
 			}
 			if b == "电磁泄露发射防护类型" {
-				fields["launch_type"] = c
+				tags["launch_type"] = c
 			}
 			if b == "生产者（制造商）" {
-				fields["manufacturer"] = c
+				tags["manufacturer"] = c
 			}
 			if b == "系统版本" {
-				fields["sys_version"] = c
+				tags["sys_version"] = c
 			}
 			if b == "内核版本" {
-				fields["kernel"] = c
+				tags["kernel"] = c
 			}
 			if b == "系统位数" {
-				fields["sys_number"] = c
+				tags["sys_number"] = c
 			}
 			if b == "三合一内核版本" {
-				fields["three_kernel"] = c
+				tags["three_kernel"] = c
 			}
 			if b == "三合一软件版本" {
-				fields["three_version"] = c
+				tags["three_version"] = c
 			}
 			if b == "固件版本（BIOS）" {
-				fields["bios"] = c
+				tags["bios"] = c
 			}
 			if b == "处理器信息" {
-				fields["cpu_info"] = c
+				tags["cpu_info"] = c
 			}
 			if b == "内存" {
-				fields["memory"] = c
+				tags["memory"] = c
 			}
 			if b == "硬盘序列号" {
-				fields["disk_number"] = c
+				tags["disk_number"] = c
 			}
 			if b == "硬盘容量" {
-				fields["disk_capacity"] = c
+				tags["disk_capacity"] = c
 			}
 			if b == "主板版本号" {
-				fields["mainboard_version"] = c
+				tags["mainboard_version"] = c
 			}
 			if b == "系统更新时间" {
-				fields["sys_update_time"] = c
+				tags["sys_update_time"] = c
 			}
 		} else {
 			if err != nil { //遇到任何错误立即返回，并忽略 EOF 错误信息
@@ -97,17 +97,18 @@ func (_ *SysInfoStats) Gather(acc telegraf.Accumulator) error {
 				} else {
 					l = l + 1
 				}
-				return nil
+				break
 			}
 		}
 		if err != nil { //遇到任何错误立即返回，并忽略 EOF 错误信息
 			if err == io.EOF {
 				break
 			}
-			return nil
+			return err
 		}
 	}
-	acc.AddGauge("systeminfo", fields, nil)
+	fmt.Println("fields", fields)
+	acc.AddGauge("systeminfo", nil, tags)
 	return nil
 }
 
