7d26fffd31ce49dfba5115de3ff987495229788b
ilhxasll <liyanpeter@163.com>
2019-12-24 14:34:30 +0800

    Update systeminfo.go

--- plugins/inputs/systeminfo/systeminfo.go
+++ plugins/inputs/systeminfo/systeminfo.go
@@ -42,163 +42,123 @@ func (_ *SysInfoStats) Gather(acc telegraf.Accumulator) error {
 				c := strings.Trim(a[1], "\n")
 				if b == "产品名称" {
 					fields["pro_name"] = c
-					fmt.Println(c)
 				}
 				if b == "ProductName" {
 					fields["pro_name"] = c
-					fmt.Println(c)
 				}
 				if b == "产品型号" {
 					fields["pro_number"] = c
-					fmt.Println(c)
 				}
 				if b == "ProductModel" {
 					fields["pro_number"] = c
-					fmt.Println(c)
 				}
 				if b == "标识码（产品唯一标识）" {
 					fields["pro_code"] = c
-					fmt.Println(c)
 				}
 				if b == "ID" { //安全卡标识码（产品唯一标识）
 					fields["pro_code"] = c
-					fmt.Println(c)
 				}
 				if b == "电磁泄漏发射防护类型" {
 					fields["launch_type"] = c
-					fmt.Println(c)
 				}
 				if b == "ShelterModel" {
 					fields["launch_type"] = c
-					fmt.Println(c)
 				}
 				if b == "生产者（制造商）" {
 					fields["manufacturer"] = c
-					fmt.Println(c)
 				}
 				if b == "Producter" {
 					fields["manufacturer"] = c
-					fmt.Println(c)
 				}
 				if b == "操作系统名称" {
 					fields["os_name"] = c
-					fmt.Println(c)
 				}
 				if b == "Name" {
 					fields["os_name"] = c
-					fmt.Println(c)
 				}
 				if b == "系统版本" {
 					fields["sys_version"] = c
-					fmt.Println(c)
 				}
 				if b == "Release" {
 					fields["sys_version"] = c
-					fmt.Println(c)
 				}
 				if b == "内核版本" {
 					fields["kernel"] = c
-					fmt.Println(c)
 				}
 				if b == "Kernel" {
 					fields["kernel"] = c
-					fmt.Println(c)
 				}
 				if b == "系统位数" {
 					fields["sys_number"] = c
-					fmt.Println(c)
 				}
 				if b == "Bit" {
 					fields["sys_number"] = c
-					fmt.Println(c)
 				}
 				if b == "I/O保密管理模块" {
 					fields["io_sec_model"] = c
-					fmt.Println(c)
 				}
 				if b == "安全卡版本" {
 					fields["safe_number"] = c
-					fmt.Println(c)
 				}
 				if b == "Version" {
 					fields["safe_number"] = c
-					fmt.Println(c)
 				}
 				if b == "固件版本（BIOS）" {
 					fields["bios"] = c
-					fmt.Println(c)
 				}
 				if b == "BiosVersion" {
 					fields["bios"] = c
-					fmt.Println(c)
 				}
 				if b == "处理器信息" {
 					fields["cpu_info"] = c
-					fmt.Println(c)
 				}
 				if b == "CPU" {
 					fields["cpu_info"] = c
-					fmt.Println(c)
 				}
 				if b == "内存" {
 					fields["memory"] = c
-					fmt.Println(c)
 				}
 				if b == "Memory" {
 					fields["memory"] = c
-					fmt.Println(c)
 				}
 				if b == "硬盘序列号" {
 					fields["disk_number"] = c
-					fmt.Println(c)
 				}
 				if b == "HDSerial" {
 					fields["disk_number"] = c
-					fmt.Println(c)
 				}
 				if b == "硬盘容量" {
 					fields["disk_capacity"] = c
-					fmt.Println(c)
 				}
 				if b == "HDCapacity" {
 					fields["disk_capacity"] = c
-					fmt.Println(c)
 				}
 				if b == "主板版本号" {
 					fields["mainboard_version"] = c
-					fmt.Println(c)
 				}
 				if b == "系统安装时间" {
 					fields["sys_begin_time"] = c
-					fmt.Println(c)
 				}
 				if b == "系统更新时间" {
 					fields["sys_update_time"] = c
-					fmt.Println(c)
 				}
 				if b == "UpdateTime" {
 					fields["sys_update_time"] = c
-					fmt.Println(c)
 				}
 				if b == "KernelVersion" { //三合一内核版本
 					fields["three_kernel"] = c
-					fmt.Println(c)
 				}
 				if b == "SoftWareVersion" { //三合一软件版本
 					fields["three_version"] = c
-					fmt.Println(c)
 				}
 				if b == "Product" { //操作系统名称
 					fields["sys_product"] = c
-					fmt.Println(c)
 				}
 				if b == "HDSerial_1" { //HOME 硬盘序列号
 					fields["home_disk_number"] = c
-					fmt.Println(c)
 				}
 				if b == "HDCapacity_1" { //HOME 硬盘容量
 					fields["home_disk_capacity"] = c
-					fmt.Println(c)
 				}
 			} else {
 				if err != nil { //遇到任何错误立即返回，并忽略 EOF 错误信息
