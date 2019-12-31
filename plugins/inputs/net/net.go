108c1f913dd950a4b618d5c738a5d00e3bc5f1b2
ilhxasll <liyanpeter@163.com>
2019-12-31 11:01:36 +0800

    Update net.go

--- plugins/inputs/net/net.go
+++ plugins/inputs/net/net.go
@@ -1,16 +1,9 @@
 package net
 
 import (
-	"bufio"
 	"fmt"
-	"log"
 	"net"
-	"os/exec"
-	"regexp"
-	"strconv"
 	"strings"
-
-	"github.com/safchain/ethtool"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/filter"
@@ -18,8 +11,7 @@ import (
 	"github.com/influxdata/telegraf/plugins/inputs/system"
 )
 
-//zhaojyun smnet
-type SMNetIOStats struct {
+type NetIOStats struct {
 	filter filter.Filter
 	ps     system.PS
 
@@ -28,28 +20,11 @@ type SMNetIOStats struct {
 	Interfaces          []string
 }
 
-//zhaojianyun 接口最大网速与网路接口状态
-type InterfacesSpeed struct {
-	Index      int
-	MTU        int
-	Name       string
-	MacAddress string
-	Ipv4       []string
-	Mask       []string
-	Flags      []string
-}
-
-//zhaojianyun 接口最大网速与网路接口状态
-type SMIORunStatus struct {
-	RunStatus uint32
-	Speed     uint64
-}
-
-func (_ *SMNetIOStats) Description() string {
+func (_ *NetIOStats) Description() string {
 	return "Read metrics about network interface usage"
 }
 
-var smNetSampleConfig = `
+var netSampleConfig = `
   ## By default, telegraf gathers stats from any up interface (excluding loopback)
   ## Setting interfaces will tell it to gather these explicit interfaces,
   ## regardless of status.
@@ -63,14 +38,14 @@ var smNetSampleConfig = `
   ##
 `
 
-func (_ *SMNetIOStats) SampleConfig() string {
-	return smNetSampleConfig
+func (_ *NetIOStats) SampleConfig() string {
+	return netSampleConfig
 }
 
-func (s *SMNetIOStats) Gather(acc telegraf.Accumulator) error {
+func (s *NetIOStats) Gather(acc telegraf.Accumulator) error {
 	netio, err := s.ps.NetIO()
 	if err != nil {
-
+		return fmt.Errorf("error getting net io info: %s", err)
 	}
 
 	if s.filter == nil {
@@ -83,15 +58,10 @@ func (s *SMNetIOStats) Gather(acc telegraf.Accumulator) error {
 	if err != nil {
 		return fmt.Errorf("error getting list of interfaces: %s", err)
 	}
-
-	//网络接口map
 	interfacesByName := map[string]net.Interface{}
 	for _, iface := range interfaces {
 		interfacesByName[iface.Name] = iface
 	}
-
-	//获取网关信息
-	gateways := ReadGateways()
 
 	for _, io := range netio {
 		if len(s.Interfaces) != 0 {
@@ -123,38 +93,7 @@ func (s *SMNetIOStats) Gather(acc telegraf.Accumulator) error {
 			"interface": io.Name,
 		}
 
-		tiface, _ := interfacesByName[io.Name]
-
-		//解析mask地址
-		ip, mask, _ := ParseIPMask(tiface)
-
-		//接口配置状态
-		var adminStatus uint32
-		flags := strings.Split(tiface.Flags.String(), "|")
-		if len(flags) > 0 {
-			if strings.ReplaceAll(flags[0], " ", "") == "up" {
-				adminStatus = 1
-			}
-		}
-		//接口运行状态和网速
-		gateway, ok := gateways[io.Name]
-		if !ok {
-			gateway = "---"
-		}
-		instates := ReadRunStatus(io.Name)
-
 		fields := map[string]interface{}{
-			//"sdd" : iface.
-			"index":        tiface.Index,
-			"name":         tiface.Name,
-			"mtu":          tiface.MTU,
-			"speed":        instates.Speed,
-			"ip":           ip,
-			"net_mask":     mask,
-			"gateway":      gateway,
-			"mac":          tiface.HardwareAddr.String(),
-			"admin_status": adminStatus,
-			"run_status":   instates.RunStatus,
 			"bytes_sent":   io.BytesSent,
 			"bytes_recv":   io.BytesRecv,
 			"packets_sent": io.PacketsSent,
@@ -164,7 +103,7 @@ func (s *SMNetIOStats) Gather(acc telegraf.Accumulator) error {
 			"drop_in":      io.Dropin,
 			"drop_out":     io.Dropout,
 		}
-		acc.AddCounter("smnet", fields, tags)
+		acc.AddCounter("net", fields, tags)
 	}
 
 	// Get system wide stats for different network protocols
@@ -182,157 +121,14 @@ func (s *SMNetIOStats) Gather(acc telegraf.Accumulator) error {
 		tags := map[string]string{
 			"interface": "all",
 		}
-		acc.AddFields("smnet", fields, tags)
+		acc.AddFields("net", fields, tags)
 	}
 
 	return nil
 }
 
 func init() {
-	inputs.Add("smnet", func() telegraf.Input {
-		return &SMNetIOStats{ps: system.NewSystemPS()}
+	inputs.Add("net", func() telegraf.Input {
+		return &NetIOStats{ps: system.NewSystemPS()}
 	})
-
-}
-
-/*
- * 函数名： ParseIPMask(iface net.Interface)
- * 作用：根据IP解析Mask地址
- * 返回值：IP地址，MASK地址
- */
-func ParseIPMask(iface net.Interface) (string, string, error) {
-	adds, err := iface.Addrs()
-	if err != nil {
-		log.Fatal("get network addr failed: ", err)
-		return "", "", err
-	}
-	ipv4 := "--"
-	mask := "--"
-	for _, ip := range adds {
-		if strings.Contains(ip.String(), ".") {
-			_, ipNet, err := net.ParseCIDR(ip.String())
-			if err != nil {
-				fmt.Println(err)
-			}
-			val := make([]byte, len(ipNet.Mask))
-			copy(val, ipNet.Mask)
-			var s []string
-			for _, i := range val[:] {
-				s = append(s, strconv.Itoa(int(i)))
-			}
-			ipv4 = ip.String()[:strings.Index(ip.String(), "/")]
-			mask = strings.Join(s, ".")
-			break
-		}
-	}
-	return ipv4, mask, nil
-}
-
-/*
- * 函数名：delete_extra_space(s string) string
- * 功  能:删除字符串中多余的空格(含tab)，有多个空格时，仅保留一个空格，同时将字符串中的tab换为空格
- * 参  数:s string:原始字符串
- * 返回值:string:删除多余空格后的字符串
- */
-func deleteExtraSpace(s string) string {
-	//删除字符串中的多余空格，有多个空格时，仅保留一个空格
-	s1 := strings.Replace(s, "	", " ", -1)       //替换tab为空格
-	regstr := "\\s{2,}"                          //两个及两个以上空格的正则表达式
-	reg, _ := regexp.Compile(regstr)             //编译正则表达式
-	s2 := make([]byte, len(s1))                  //定义字符数组切片
-	copy(s2, s1)                                 //将字符串复制到切片
-	spc_index := reg.FindStringIndex(string(s2)) //在字符串中搜索
-	for len(spc_index) > 0 {                     //找到适配项
-		s2 = append(s2[:spc_index[0]+1], s2[spc_index[1]:]...) //删除多余空格
-		spc_index = reg.FindStringIndex(string(s2))            //继续在字符串中搜索
-	}
-	return string(s2)
-}
-
-/*
- * 函数名：Readgateways() map[string]string
- * 作用：读取网关信息
- * 返回值：map[网络接口名]网关地址
- */
-func ReadGateways() map[string]string {
-	cmd := exec.Command("route", "-n")
-	//创建获取命令输出管道
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Printf("Error:can not obtain stdout pipe for command:%s\n", err)
-	}
-	//执行命令
-	if err := cmd.Start(); err != nil {
-		fmt.Println("Error:The command is err,", err)
-	}
-	//使用带缓冲的读取器
-	outputBuf := bufio.NewReader(stdout)
-	var gateways map[string]string /*创建集合 */
-	gateways = make(map[string]string)
-	var i int
-	for {
-		//一次获取一行,_ 获取当前行是否被读完
-		output, _, err := outputBuf.ReadLine()
-		if err != nil {
-			// 判断是否到文件的结尾了否则出错
-			if err.Error() != "EOF" {
-				fmt.Printf("Error :%s\n", err)
-			}
-			break
-		}
-		if i < 2 {
-			i++
-			continue
-		}
-		tempgate := strings.Split(deleteExtraSpace(string(output)), " ")
-
-		if len(tempgate) == 8 {
-
-			_, ok := gateways[tempgate[7]]
-			if ok {
-				continue
-			}
-			gateways[tempgate[7]] = tempgate[1]
-		}
-	}
-
-	//wait 方法会一直阻塞到其所属的命令完全运行结束为止
-	if err := cmd.Wait(); err != nil {
-		fmt.Println("wait:", err.Error())
-	}
-
-	return gateways
-}
-
-/*
- * 函数名： ReadRunStatus(ifacename string)
- * 作用：获取接口运行状态和网速
- * 返回值：SMIORunStatus
- */
-func ReadRunStatus(ifacename string) SMIORunStatus {
-
-	//获取ethtool命令句柄
-	ethHandle, err := ethtool.NewEthtool()
-	if err != nil {
-		panic(err.Error())
-	}
-	defer ethHandle.Close()
-
-	var instates SMIORunStatus
-
-	//获取接口运行状态
-	stats, err := ethHandle.LinkState(ifacename)
-	if err == nil {
-		instates.RunStatus = stats
-	}
-
-	//获取网速
-	result, err := ethHandle.CmdGetMapped(ifacename)
-	if err == nil {
-		speed, ok := result["speed"]
-		if ok && speed != 4294967295 {
-			instates.Speed = speed
-		}
-	}
-	return instates
 }
